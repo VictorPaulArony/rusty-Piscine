71ed8cd6208005a593620b05336d64ab2ab3f7e1
victorpaularony <smallypauls@gmail.com>
2025-04-01 17:46:11 +0300

    DELETED: target and cargo.lock

--- scalar/target/debug/deps/scalar-e5a77a3b9c09004d.d
+++
@@ -1,5 +0,0 @@
-/home/viarony/Desktop/rusty-Piscine/scalar/target/debug/deps/scalar-e5a77a3b9c09004d: src/lib.rs
-
-/home/viarony/Desktop/rusty-Piscine/scalar/target/debug/deps/scalar-e5a77a3b9c09004d.d: src/lib.rs
-
-src/lib.rs:
--- scalar/target/debug/libscalar.d
+++
@@ -1,1 +0,0 @@
-/home/viarony/Desktop/rusty-Piscine/scalar/target/debug/libscalar.rlib: /home/viarony/Desktop/rusty-Piscine/scalar/src/lib.rs
