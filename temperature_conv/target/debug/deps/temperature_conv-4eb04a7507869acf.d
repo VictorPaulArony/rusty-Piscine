9c6180364784b532e9ceef7ae6f6baf44dc7b551
victorpaularony <smallypauls@gmail.com>
2025-04-01 23:26:40 +0300

    Stop tracking Cargo.lock

--- temperature_conv/target/debug/deps/temperature_conv-4eb04a7507869acf.d
+++
@@ -1,7 +0,0 @@
-/home/viarony/Desktop/rusty-Piscine/temperature_conv/target/debug/deps/libtemperature_conv-4eb04a7507869acf.rmeta: src/lib.rs
-
-/home/viarony/Desktop/rusty-Piscine/temperature_conv/target/debug/deps/libtemperature_conv-4eb04a7507869acf.rlib: src/lib.rs
-
-/home/viarony/Desktop/rusty-Piscine/temperature_conv/target/debug/deps/temperature_conv-4eb04a7507869acf.d: src/lib.rs
-
-src/lib.rs:
