ce0631adafca3a35dbcf014799dafbedc0d8dc08
victorpaularony <smallypauls@gmail.com>
2025-04-01 23:48:33 +0300

    Remove Cargo.lock from tracking

--- scalar/src/target/debug/deps/src-65830adfda3983ba.d
+++
@@ -1,5 +0,0 @@
-/home/viarony/Desktop/rusty-Piscine/scalar/src/target/debug/deps/src-65830adfda3983ba: lib.rs
-
-/home/viarony/Desktop/rusty-Piscine/scalar/src/target/debug/deps/src-65830adfda3983ba.d: lib.rs
-
-lib.rs:
--- scalar/src/target/debug/deps/src-bab5041aa825bd4e.d
+++
@@ -1,7 +0,0 @@
-/home/viarony/Desktop/rusty-Piscine/scalar/src/target/debug/deps/libsrc-bab5041aa825bd4e.rmeta: lib.rs
-
-/home/viarony/Desktop/rusty-Piscine/scalar/src/target/debug/deps/libsrc-bab5041aa825bd4e.rlib: lib.rs
-
-/home/viarony/Desktop/rusty-Piscine/scalar/src/target/debug/deps/src-bab5041aa825bd4e.d: lib.rs
-
-lib.rs:
