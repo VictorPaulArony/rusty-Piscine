df1b0aabe53daa7dc70c54f3517b9954987ba29b
victorpaularony <smallypauls@gmail.com>
2025-04-01 18:02:30 +0300

    UPDATE: add all the files using gitignore

---
+++ scalar/src/target/debug/deps/src-65830adfda3983ba.d
@@ -0,0 +1,5 @@
+/home/viarony/Desktop/rusty-Piscine/scalar/src/target/debug/deps/src-65830adfda3983ba: lib.rs
+
+/home/viarony/Desktop/rusty-Piscine/scalar/src/target/debug/deps/src-65830adfda3983ba.d: lib.rs
+
+lib.rs:
---
+++ scalar/src/target/debug/deps/src-bab5041aa825bd4e.d
@@ -0,0 +1,7 @@
+/home/viarony/Desktop/rusty-Piscine/scalar/src/target/debug/deps/libsrc-bab5041aa825bd4e.rmeta: lib.rs
+
+/home/viarony/Desktop/rusty-Piscine/scalar/src/target/debug/deps/libsrc-bab5041aa825bd4e.rlib: lib.rs
+
+/home/viarony/Desktop/rusty-Piscine/scalar/src/target/debug/deps/src-bab5041aa825bd4e.d: lib.rs
+
+lib.rs:
