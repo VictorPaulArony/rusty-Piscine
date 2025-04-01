1f286f2c93e1b6f054607d3484f6a545bc86879b
victorpaularony <smallypauls@gmail.com>
2025-04-01 23:41:48 +0300

    main

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -8,19 +8,3 @@ pub fn celsius_to_fahrenheit(c: f64) -> f64 {
 }
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_fahrenheit_to_celsius(){
-        assert_eq!(fahrenheit_to_celsius(-459.67),-273.15);
-    } 
-
-    #[test]
-    fn test_celsius_to_fahrenheit(){
-        assert_eq!(celsius_to_fahrenheit(0.0), 32.0)
-    }
-
-   
-}
