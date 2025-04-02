589e2b05d922aaf69a527791a01641c6b0f3a02b
victorpaularony <smallypauls@gmail.com>
2025-04-03 00:44:35 +0300

    functions to find,split,ascii and empty

---
+++ string_literals/src/lib.rs
@@ -0,0 +1,33 @@
+pub fn is_empty(v: &str) -> bool {
+    v.is_empty()
+}
+
+pub fn is_ascii(v: &str) -> bool {
+    v.is_ascii()
+}
+
+pub fn contains(v: &str, pat: &str) -> bool {
+    v.contains(pat)
+}
+
+pub fn split_at(v: &str, index: usize) -> (&str, &str) {
+    v.split_at(index)
+}
+
+pub fn find(v: &str, pat: char) -> usize {
+    v.find(pat).expect("REASON")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(is_empty(""), true);
+        assert_eq!(is_ascii("rust"), true);
+        assert_eq!(contains("rust", "ru"), true);
+        assert_eq!(split_at("rust", 2),("ru", "st"));
+        assert_eq!(find("rust", 'u'), 1);
+    }
+}
