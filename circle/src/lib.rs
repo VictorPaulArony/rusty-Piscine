8c3ddd2613bc1742f8a9cd3d981e6457efb910cf
victorpaularony <smallypauls@gmail.com>
2025-04-04 22:59:58 +0300

    circle/src/lib.rs

--- circle/src/lib.rs
+++ circle/src/lib.rs
@@ -1,13 +1,13 @@
 #[derive(Debug, Clone, Copy)]
 
 pub struct Circle {
-	center :(f64,f64),
+	center :Point,
 	radius :f64
 }
 
 impl Circle {
-    pub fn diameter(&self) -> f64 {
-        self.radius * 2.0 
+    pub fn diameter(&self) -> i64 {
+        (self.radius * 2.0) as i64
     }
     pub fn area(&self) -> f64 {
         std::f64::consts::PI * (self.radius *self.radius) as f64
@@ -21,25 +21,22 @@ impl Circle {
     } 
     pub fn new(x: f64, y:f64,  radius: f64) -> Self {
         Circle{
-            center:(x,y), 
+            center:Point(x,y), 
             radius: radius
         }
     }
 }
 
 //for the points
-pub struct Point{
-    a:f64,
-    b:f64
-}   
+#[derive(Debug, Clone, Copy)]
+pub struct Point(pub f64, pub f64);
+
+    
 impl Point {
     pub fn distance(&self, other: &Point) -> f64 {
-        let da = (self.a - other.a) as f64;
-        let db = (self.b - other.b) as f64;
+        let da = (self.0 - other.0) as f64;
+        let db = (self.1 - other.1) as f64;
         (da * da + db * db).sqrt()
-    }
-    pub fn new(a: f64, b:f64) -> Self {
-        Point{a, b}
     }
 
 }
@@ -52,13 +49,20 @@ mod tests {
     #[test]
     fn it_works() {
         let circle = Circle::new(500.0, 500.0, 150.0);
-        let circle1 = Circle::new(80.0, 115.0, 30.0);
-        let point_a = Point::new(1.0, 1.0);
-        let point_b = Point::new(0.0, 0.0);
+        let circle1 = Circle {
+            center: Point(80.0, 115.0),
+            radius: 30.0,
+        };
+        let point_a = Point(1.0, 1.0);
+        let point_b = Point(0.0, 0.0);
+
+        
+        
+        
 
         assert_eq!(circle.area(), 70685.83470577035);
-        assert_eq!(circle.diameter(), 300.0);
-        assert_eq!(circle1.diameter(), 60.0);
+        assert_eq!(circle.diameter(), 300);
+        assert_eq!(circle1.diameter(), 60);
         assert_eq!(circle.intersect(&circle1), false);
         assert_eq!(point_a.distance(&point_b), 1.4142135623730951);
     }
