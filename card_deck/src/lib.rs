36d1aff7cd2a762abf910905018c6d6a179ba95d
victorpaularony <smallypauls@gmail.com>
2025-04-05 01:18:56 +0300

    card_deck/src/lib.rs

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -60,7 +60,7 @@ pub struct Card {
     pub rank: Rank,
 }
 
-pub fn winner_card(card: &Card) -> bool {
+pub fn winner_card(card: Card) -> bool {
     card.suit == Suit::Spade && card.rank == Rank::Ace
 }
 
