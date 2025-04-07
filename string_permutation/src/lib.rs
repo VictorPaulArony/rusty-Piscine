c994d295a0c39ca0992e4d8729f48af83090669a
victorpaularony <smallypauls@gmail.com>
2025-04-07 15:28:29 +0300

    function is_permutation, that returns true if the string s1 is a permutation of s2.

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,6 +1,17 @@
 pub fn is_permutation(s1: &str, s2: &str) -> bool {
-    s1.chars().count() == s2.chars().count()
-    
+  // If the lengths are different, they cannot be permutations
+  if s1.len() != s2.len() {
+    return false;
+}
+
+// Convert the strings to vectors of characters, sort them, and compare
+let mut s1_chars: Vec<char> = s1.chars().collect();
+let mut s2_chars: Vec<char> = s2.chars().collect();
+
+s1_chars.sort_unstable();
+s2_chars.sort_unstable();
+
+s1_chars == s2_chars    
 }
 
 #[cfg(test)]
@@ -9,8 +20,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let word = "thought";
-        let word1 = "thougth";
-        assert_eq!(is_permutation(word, word1), true);
+        let word = "avsde";
+        let word1 = "edbca";
+        assert_eq!(!is_permutation(word, word1), true);
     }
 }
