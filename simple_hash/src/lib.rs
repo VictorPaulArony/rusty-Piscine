eb58b2cc2ff0907dd7765f373cb62fc79b598a11
victorpaularony <smallypauls@gmail.com>
2025-04-07 14:46:39 +0300

    simple_hash/src/lib.rs

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+pub fn word_frequency_counter<'a>(words: &'a [&'a str]) -> HashMap<&'a str, usize> {
+    let mut char_count = HashMap::new();
+    for word in words {
+        let count = char_count.entry(*word).or_insert(0);
+        *count += 1;
+    }
+    char_count
+}
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+   frequency_count.len()
+
+}
+
+
+
