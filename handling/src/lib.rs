30efea137af73393bcdefcb466dc695b0971b42f
victorpaularony <smallypauls@gmail.com>
2025-04-09 15:06:21 +0300

    function to write or creat file if not found

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -8,7 +8,7 @@ use std::io::Write;
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
     match File::open(&path) {
         Ok(mut file) => {
-            writeln!(file, "{}", content).expect("Failed");
+            writeln!(file, "{}", content).unwrap();
         }
         Err(ref e) if e.kind() == ErrorKind::NotFound => {
             let mut file = File::create(&path).expect("Failed");
