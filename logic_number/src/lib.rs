18f335d4988cd6ebdc77c776aceb2f4bf10e5cca
victorpaularony <smallypauls@gmail.com>
2025-04-15 06:57:25 +0300

    UPDATE: code optimization

--- logic_number/src/lib.rs
+++ logic_number/src/lib.rs
@@ -1,11 +1,7 @@
 pub fn number_logic(num: u32) -> bool {
-    let digits: Vec<u32> = num.to_string()
-        .chars()
-        .filter_map(|c| c.to_digit(10))
-        .collect();
-
-    let len = digits.len() as u32;
-    let sum: u32 = digits.iter().map(|d| d.pow(len)).sum();
-
+    let size = num.to_string().len() as u32;
+    let st = num.to_string();
+    let sum: u32= st.chars().filter_map(|d| d.to_digit(10)).map(|n| n.pow(size)).sum();
     sum == num
+
 }
