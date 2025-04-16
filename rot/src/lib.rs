50f4e4420d2037c0dde2c6c4a92f9181c3086987
victorpaularony <smallypauls@gmail.com>
2025-04-16 06:56:18 +0300

    UPDATE: to use arithmetics

--- rot/src/lib.rs
+++ rot/src/lib.rs
@@ -1,18 +1,14 @@
 pub fn rotate(input: &str, key: i8) -> String {
-    input
-        .chars()
-        .map(|c| {
-            if c.is_ascii_lowercase() {
-                let a = b'a';
-                let offset = ((c as u8 - a + (key.rem_euclid(26) as u8)) % 26) + a;
-                offset as char
-            } else if c.is_ascii_uppercase() {
-                let a = b'A';
-                let offset = ((c as u8 - a + (key.rem_euclid(26) as u8)) % 26) + a;
-                offset as char
-            } else {
-                c
-            }
+    let res:String = input.chars()
+        .map(|c| match c{
+                'a'..='z' => {
+                    ((c as u8 - b'a' + ((key %26 + 26)%26) as u8) %26 + b'a')as char
+                }
+                'A'..='Z' => {
+                    ((c as u8 - b'A' + ((key %26 + 26)%26) as u8)%26 +b'A')as char
+                }
+                _ => c,
         })
-        .collect()
+        .collect();
+        res as String
 }
