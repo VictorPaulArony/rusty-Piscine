d866c530f83384ae656b6b9d56c21ffac7da786b
victorpaularony <smallypauls@gmail.com>
2025-04-16 17:21:47 +0300

    interchanged the structs

--- question_mark/src/lib.rs
+++ question_mark/src/lib.rs
@@ -1,18 +1,18 @@
+pub struct Four {
+    pub fourth_layer: Option<u16>,
+}
+pub struct Three {
+    pub third_layer: Option<Four>,
+}
+pub struct Two {
+    pub second_layer: Option<Three>,
+}
 pub struct One {
     pub first_layer: Option<Two>,
 }
 
-pub struct Two {
-    pub second_layer: Option<Three>,
-}
 
-pub struct Three {
-    pub third_layer: Option<Four>,
-}
 
-pub struct Four {
-    pub fourth_layer: Option<u16>,
-}
 
 impl One {
     pub fn get_fourth_layer(self) -> Option<u16> {
